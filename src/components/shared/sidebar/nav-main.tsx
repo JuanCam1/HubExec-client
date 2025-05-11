0a1908559d85434501d7c9b54d16c34a9f08cf14
Juan Camilo Rojas <juancamilor576@gmail.com>
2025-05-10 22:32:06 -0500

    fix: diseño del modo light y dark

--- src/components/shared/sidebar/nav-main.tsx
+++ src/components/shared/sidebar/nav-main.tsx
@@ -20,11 +20,11 @@ const NavMain = () => {
           <SidebarMenuItem className="mt-2" key={item.title}>
             <Link to={item.url}>
               <SidebarMenuButton
-                className={`text-sidebar-foreground/70  cursor-pointer ${item.url.includes(location.pathname) && "bg-zinc-800/80"}`}
+                className={` hover:bg-zinc-300 dark:hover:bg-zinc-800/90 cursor-pointer ${item.url.includes(location.pathname) && "bg-zinc-300 dark:bg-zinc-800/90"}`}
               >
-                <item.icon className="text-sidebar-foreground/70" />
+                <item.icon className="text-zinc-800 dark:text-white" />
                 <span
-                  className={`font-medium text-zinc-600 dark:text-white ${location.pathname.includes(item.url) && "font-bold text-blue-500"}`}
+                  className={`font-medium text-zinc-800 dark:text-white ${location.pathname.includes(item.url) && "font-bold text-blue-500"}`}
                 >
                   {item.title}
                 </span>
